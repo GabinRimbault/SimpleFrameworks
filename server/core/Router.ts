789b4abff37df67fb12959b0d1b88c83c8530c49
Amiltone Niort <gabin.rimbault@yahoo.fr>
2019-06-22 20:32:19 +0200

    Finalisation Router + en attente de dev des controller/models

--- server/core/Router.ts
+++ server/core/Router.ts
@@ -1,8 +1,6 @@
 import { routerError } from '../config/error/router.error';
-import { IPluginsVar } from './libs/interface/IPluginsVar';
-import { Plugins } from './libs/class/Plugins';
 import { Logs } from './libs/class/Logs';
-
+const chalk = require('chalk')
 
 export class Router {
 
@@ -20,23 +18,14 @@ export class Router {
 
     public init(): void{
         this.loadPlugin()
-        console.log('NB Plugins load : ', this.path.length)
+        console.log('NB Plugins load : ', chalk.green(this.path.length))
     }
 
-    private loadPlugin(): boolean{
-
-        let plugins: any = {
-            name: '',
-            get: [],
-            post: []
-        }
-
+    private loadPlugin(): void{
 
         for(let i = 0; i !=  this.path.length; i++){
 
-            plugins['name'] = this.path[i].name
-
-            console.log('[ ' + this.path[i].name.toUpperCase() + ' ]')
+            console.log("[ " + chalk.cyan(this.path[i].name.toUpperCase()) + " ]")
 
             for(let n = 0; n != this.path[i].path.length; n++){
                 //METHODS GET
@@ -48,92 +37,86 @@ export class Router {
                 //METHODS ¨POST
                 this.path[i].path[n].method === 'post' ? 
                 Logs.tryCatch(() => {
-                    this.loadMethodPost(this.path[i].path[n], this)
+                    this.loadMethodPost(this.path[i].path[n])
                 }, {}) : ''
 
                 //METHODS PUT
                 this.path[i].path[n].method === 'put' ? 
                 Logs.tryCatch(() => {
-                    this.loadMethodPut(this.path[i].path[n], this)
+                    this.loadMethodPut(this.path[i].path[n])
                 }, {}) : ''
 
                 //METHODS DEL
                 this.path[i].path[n].method === 'del' ? 
                 Logs.tryCatch(() => {
-                    this.loadMethodDel(this.path[i].path[n], this)
+                    this.loadMethodDel(this.path[i].path[n])
                 }, {}) : ''
 
                 //METHODS PATCH
                 this.path[i].path[n].method === 'patch' ? 
                 Logs.tryCatch(() => {
-                    this.loadMethodPatch(this.path[i].path[n], this)
+                    this.loadMethodPatch(this.path[i].path[n])
                 }, {}) : ''
             }
             console.log("---------------------------------------")
         }
-        
-        return true
     }
 
     private loadMethodGet(path: object): any {
         //If not protected
         if(!path.protected){
-            console.log("GET { " + path.path + " }")
+            console.log(chalk.green("GET") + " { " + path.path + " }")
             this.server.get('/' + path.path, ((req, res) => {
                 console.log('Debugger ')
               }))
         }else{
-            console.log("Protected [ GET { " + path.path + " } ]")
+            console.log(chalk.green("GET") + " { " + path.path + " } - " + chalk.magenta("[GUARD]"))
             this.server.get('/' + path.path, ((req, res) => {}))
         }
     }
 
-    private loadMethodPost(path: object, self: any): void {
+    private loadMethodPost(path: object): void {
         //If not protected
         if(!path.protected){
-            console.log("POST { " + path.path + " }")
+            console.log(chalk.blue("POST") + " { " + path.path + " }")
             this.server.post('/' + path.path, ((req, res) => {}))
         }else{
-            console.log("Protected [ POST { " + path.path + " } ]")
+            console.log(chalk.blue("POST") + " { " + path.path + " } - " + chalk.magenta("[GUARD]"))
             this.server.post('/' + path.path, ((req, res) => {}))
         }
     }
 
-    private loadMethodPut(path: object, self: any): void {
+    private loadMethodPut(path: object): void {
         //If not protected
         if(!path.protected){
-            console.log("PUT { " + path.path + " }")
+            console.log(chalk.yellow("PUT") + " { " + path.path + " }")
             this.server.put('/' + path.path, ((req, res) => {}))
         }else{
-            console.log("Protected [ PUT { " + path.path + " } ]")
+            console.log(chalk.yellow("PUT") + " { " + path.path + " } - " + chalk.magenta("[GUARD]"))
             this.server.put('/' + path.path, ((req, res) => {}))
         }
     }
 
-    private loadMethodDel(path: object, self: any): void {
+    private loadMethodDel(path: object): void {
         //If not protected
         if(!path.protected){
-            console.log("DEL { " + path.path + " }")
-            this.server.del('/' + path.path, ((req, res) => {}))
+            console.log(chalk.red("DEL") + " { " + path.path + " }")
+            this.server.delete('/' + path.path, ((req, res) => {}))
         }else{
-            console.log("Protected [ DEL { " + path.path + " } ]")
-            this.server.del('/' + path.path, ((req, res) => {}))
+            console.log(chalk.red("DEL") + " { " + path.path + " } - " + chalk.magenta("[GUARD]"))
+            this.server.delete('/' + path.path, ((req, res) => {}))
+
         }
     }
 
-    private loadMethodPatch(path: object, self: any): void {
+    private loadMethodPatch(path: object): void {
         //If not protected
         if(!path.protected){
-            console.log("Patch { " + path.path + " }")
+            console.log(chalk.green("PATCH") + " { " + path.path + " }")
             this.server.post('/' + path.path, ((req, res) => {}))
         }else{
-            console.log("Protected [ Patch { " + path.path + " } ]")
+            console.log(chalk.green("PATCH") + " { " + path.path + " } - " + chalk.magenta("[GUARD]"))
             this.server.patch('/' + path.path, ((req, res) => {}))
         }
     }
-
-
-
-
-
 }
--- server/core/libs/interface/IPluginsVar.ts
+++
@@ -1,4 +0,0 @@
-export interface IPluginsVar{
-    name: string,
-    
-}
